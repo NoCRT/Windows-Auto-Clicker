58279e8f0d7bfb8db62db0c399dce14d563fb57f
NoCRT <39925234+NoCRT@users.noreply.github.com>
2020-07-13 02:12:35 +0100

    added ability to change speed between a safe range

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <SFML/Window.hpp>
 #include <SFML/Audio.hpp>
-#include "random.hpp"
 #include <iostream>
+#include <thread>
 #include <windows.h>
 
-using random = effolkronium::random_static;
+//using random = effolkronium::random_static;
 
 void click()
 {
@@ -22,14 +22,16 @@ void click()
     Input.mi.dwFlags = MOUSEEVENTF_LEFTUP;
     ::SendInput(1, &Input, sizeof(INPUT));
 
-    // left down
+    // this is neeeded for sfml to detect that the macro is still running
+    // this causes some isues that need to be resolved like firing the loop when it
+    // shouldn't, This also adds an extra click to the end of the loop.
     Input.type = INPUT_MOUSE;
     Input.mi.dwFlags = MOUSEEVENTF_LEFTDOWN;
     ::SendInput(1, &Input, sizeof(INPUT));
 }
 
-// auto_on.wav
-// auto_off.wav
+// auto_on.wav : https://freesound.org/people/deleted_user_4772965/sounds/256839/
+// auto_off.wav : https://freesound.org/people/RutgerMuller/sounds/51168/
 
 int main()
 {
@@ -41,12 +43,16 @@ int main()
     toggle_on_buffer.loadFromFile("auto_on.wav");
     sf::Sound toggle_on(toggle_on_buffer);
 
+    int delay = 90;
+
     bool toggle = false;
     std::clog << "STATUS : OFF";
+    
     while (true)
     {
-        static sf::Clock timer;
-        if (timer.getElapsedTime().asSeconds() > 1)
+        // toggle functionality on or off
+        static sf::Clock toggle_timer;
+        if (toggle_timer.getElapsedTime().asSeconds() > 1)
         {
             if (!toggle && sf::Keyboard::isKeyPressed(sf::Keyboard::Home))
             {
@@ -54,7 +60,7 @@ int main()
                 toggle = true;
                 system("cls");
                 std::clog << "STATUS : ON";
-                timer.restart();
+                toggle_timer.restart();
             }
             else if (toggle && sf::Keyboard::isKeyPressed(sf::Keyboard::Home))
             {
@@ -62,16 +68,49 @@ int main()
                 toggle = false;
                 system("cls");
                 std::clog << "STATUS : OFF";
-                timer.restart();
+                toggle_timer.restart();
             }
         }
 
+        // increase and decrease delay inbetween shots
+        static sf::Clock delay_timer;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::PageUp))
+        {
+            if (delay <= 120 && delay_timer.getElapsedTime().asMilliseconds() >= 250)
+            {
+                delay += 5;
+                delay_timer.restart();
+            }
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::PageDown))
+        {
+            if (delay >= 60 && delay_timer.getElapsedTime().asMilliseconds() >= 250)
+            {
+                delay -= 5;
+                delay_timer.restart();
+            }
+        }
+        
+
         if (toggle && sf::Mouse::isButtonPressed(sf::Mouse::Left))
         {
-            click();
+            static sf::Clock click_timer;
+
+            // there is a weird bug where if the mouse is clicked and unclicked and dragged quick enough it
+            // still fires LEFT_CLICK even though its not pysicaly pressed.
+            // this might be because too many events were being fired off and some errer happens somwhere
+            // as this is reduced significantly with a slower click rate.
+            //if (click_timer.getElapsedTime().asMilliseconds() > 80) // 23cps : max = 180cps @ 10
+            
+            if (click_timer.getElapsedTime().asMilliseconds() > delay) // 23cps : max = 180cps @ 10
+            {
+                click();
+                click_timer.restart();
+            }
         }
 
-        Sleep(random::get(1, 5));
+        //std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        Sleep(10);
     }
 
     std::cout << "exited loop\n";
